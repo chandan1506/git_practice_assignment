f57d8bd9eab0eda41b4c2cfd58473b2572600107
unknown <ck893607@gmail.com>
2022-09-16 23:07:43 +0530

    coded prime no and palindrome

---
+++ login.js
@@ -0,0 +1,19 @@
+                     //problem-1
+let num=13;
+let fact=0;
+for(i=1;i<=num;i++){
+if(num%i==0){
+fact++;
+}
+}
+if(fact==0){
+console.log(true)
+
+                      // problem-2
+let str="naman"
+let bag="";
+for(i=1;i<=str.length-1;i++){
+bag+=str[i];
+}
+if(bag==str){
+console.log(true)
